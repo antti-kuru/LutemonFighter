0a785af058472c8796bca3d28cc74addc4821c73
antti-kuru <antti.i.kuru@gmail.com>
2023-04-25 11:08:40 +0300

    gg

--- app/src/main/java/com/example/lutemonfighter/MainActivity.java
+++ app/src/main/java/com/example/lutemonfighter/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.lutemonfighter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
     }
+
+    public void switchToAddLutemon(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+
+    }
+    public void switchToMoveLutemon(View view){
+        Intent intent = new Intent(this, MoveLutemonActivity.class);
+        startActivity(intent);
+
+    }
+    public void switchToTrainingArea(View view){
+        Intent intent = new Intent(this, TrainingAreaActivity.class);
+        startActivity(intent);
+
+    }
+    public void switchToBattleField(View view){
+        Intent intent = new Intent(this, BattleFieldActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
+
 }
