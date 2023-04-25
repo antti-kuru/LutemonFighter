a8a4911172ee8a384a5113ea4957d793d90481d2
antti-kuru <antti.i.kuru@gmail.com>
2023-04-25 10:37:19 +0300

    gg

--- app/src/main/java/com/example/lutemonfighter/Lutemon.java
+++ app/src/main/java/com/example/lutemonfighter/Lutemon.java
@@ -1,4 +1,38 @@
 package com.example.lutemonfighter;
 
 public class Lutemon {
+    protected String name;
+    protected String color;
+    protected int image;
+    protected int attack;
+    protected int superAttack;
+    protected int defence;
+    protected int experience;
+    protected int health;
+    protected int maxHealth;
+    protected int id;
+    protected int idCounter;
+    protected int wins;
+    protected int fights;
+    protected int trainingSessions;
+
+
+    public Lutemon(){
+
+    }
+
+    public void defence(Lutemon lutemon){
+
+    }
+
+    public int attack(){
+       return attack;
+    }
+
+    public int getNumberOfCreatedLutemons(){
+        return idCounter;
+    }
+
+
+
 }
