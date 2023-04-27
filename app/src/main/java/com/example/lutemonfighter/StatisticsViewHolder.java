56f9c793769b3dd049f9aca8ccdee731bb646316
xJuhoo <juho.rekonen@student.lut.fi>
2023-04-27 15:18:03 +0300

    Stats not working but they will

---
+++ app/src/main/java/com/example/lutemonfighter/StatisticsViewHolder.java
@@ -0,0 +1,30 @@
+package com.example.lutemonfighter;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StatisticsViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView lutemonPicture;
+    TextView lutemonNameAndColor, lutemonAttack, lutemonDefense;
+    TextView lutemonHealth, lutemonExperience, lutemonWins;
+    TextView lutemonLosses, lutemonBattles, lutemonTrainings;
+
+    public StatisticsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        lutemonPicture = itemView.findViewById(R.id.imageDefaultIcon);
+        lutemonNameAndColor = itemView.findViewById(R.id.statsNameAndColor);
+        lutemonAttack = itemView.findViewById(R.id.statsAttack);
+        lutemonDefense = itemView.findViewById(R.id.statsDefense);
+        lutemonHealth = itemView.findViewById(R.id.statsHealthPoints);
+        lutemonExperience = itemView.findViewById(R.id.statsExperiencePoints);
+        lutemonWins = itemView.findViewById(R.id.statsBattlesWon);
+        lutemonLosses = itemView.findViewById(R.id.statsBattlesLost);
+        lutemonBattles = itemView.findViewById(R.id.statsBattlingSessions);
+        lutemonTrainings = itemView.findViewById(R.id.statsTrainingSessions);
+    }
+}
